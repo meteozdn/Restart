081990bf63683bf68c8bd4eb2a66b91112feb33b
Metehan Özden <metehanozden.bil@gmail.com>
2024-10-18 19:16:39 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Metehan Özden on 18.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
