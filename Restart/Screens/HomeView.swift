6db2cc91fa3c1ac7041789edc72d2f9166f05efe
Metehan Özden <metehanozden.bil@gmail.com>
2024-10-20 19:13:30 +0300

    project complated

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -8,8 +8,56 @@
 import SwiftUI
 
 struct HomeView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    @State private var isAnimating: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20){
+            //MARK: -HEADER
+            Spacer()
+            ZStack{
+                CircleGroupView(shapeColor: Color("ColorBlue"), shapeOpacity: 0.2)
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .offset(y: isAnimating ? 35 : -35)
+                    .animation(.easeInOut(duration: 4)
+                        .repeatForever()
+                        ,value: isAnimating
+                    )
+            }
+
+            //MARK: -CENTER
+            Text("The time that leads to mastery is dependent on the intensity of the focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            //MARK: -FOOTER
+            Spacer()
+            
+
+            Button {
+                withAnimation {
+                    isOnboardingViewActive = true
+                }
+                
+            } label: {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            }.buttonStyle(.borderedProminent)
+                .buttonBorderShape(.capsule)
+                .controlSize(.large)
+
+        }.onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                isAnimating = true
+            })
+        }
     }
 }
 
