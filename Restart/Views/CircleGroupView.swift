0486f72ee0fabf877282a91d3d07ff59087f5c1c
Metehan Özden <metehanozden.bil@gmail.com>
2024-10-20 19:13:13 +0300

    project Complated

---
+++ Restart/Views/CircleGroupView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleGroupView.swift
+//  Restart
+//
+//  Created by Metehan Özden on 19.10.2024.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleGroupView()
+}
